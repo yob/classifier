3a86f2287e421214c8505e2d4f039a9f89592e9e
James Healy <james@yob.id.au>
2017-08-06 00:10:54 +1000

    Alter classifier contract to accept Array's of features
    Allows us to train the FOR classifier with specific topic titles,
    rather than mashing all the topic titles together into a bag of words.
    
    The classifier can continue to operate as a basic "bag of words" classifier
    by splitting a string into an Array of words before passing it in

--- lib/classifier/naive_bayes.rb
+++ lib/classifier/naive_bayes.rb
@@ -10,31 +10,33 @@ module Classifier
       raise ArgumentError, "need 2 or more categories" if categories.size < 2
       @data = {}
       @priori = {}
-      @words = {}
+      @features = {}
       categories.each do |category|
         @data[category.to_sym] = []
         @priori[category.to_sym] = 0
-        @words[category.to_sym] = Hash.new(0)
+        @features[category.to_sym] = Hash.new(0)
       end
     end
 
-    def train(category, text)
-      raise ArgumentError, "invalid category" unless @words.has_key?(category)
+    def train(category, *features)
+      raise ArgumentError, "invalid category" unless @features.has_key?(category)
       
-      @data[category] << text.to_s.downcase.strip
-      text.split(/\s+/).each do |word|
-        @words[category][word.downcase] += 1
+      features = filter(features)
+
+      @data[category] << features
+      features.each do |feature|
+        @features[category][feature] += 1
       end
       calc_priori
     end
 
-    def classify(text)
+    def classify(*candidate_features)
       probabilities = {}
       categories.each { |cat| probabilities[cat] = @priori.fetch(cat) }
-      text.to_s.downcase.strip.split(/\s+/).each do |word|
+      filter(candidate_features).each do |feature|
         categories.each do |category|
-          word_probability = (count_word_in_category(category, word) + 1) / (total_words_in_category(category) + total_uniq_words)
-          probabilities[category] *= word_probability
+          feature_probability = (count_feature_in_category(category, feature) + 1) / (total_features_in_category(category) + total_uniq_features)
+          probabilities[category] *= feature_probability
         end
       end
       Classifier::Result.new(probabilities)
@@ -42,23 +44,31 @@ module Classifier
 
     private
 
+    def filter(features)
+      features.flatten.map { |f|
+        f.to_s.downcase.strip
+      }.reject { |f|
+        f.nil? || f == "".freeze
+      }
+    end
+
     def categories
       @data.keys
     end
 
-    def count_word_in_category(category, word)
+    def count_feature_in_category(category, feature)
       BigDecimal.new(
-        @words.fetch(category).fetch(word, 0)
+        @features.fetch(category).fetch(feature, 0)
       )
     end
     
-    def total_words_in_category(category)
-      @words.fetch(category).values.inject(BigDecimal.new(0)) { |accum, count| accum += count }
+    def total_features_in_category(category)
+      @features.fetch(category).values.inject(BigDecimal.new(0)) { |accum, count| accum += count }
     end
     
-    def total_uniq_words
+    def total_uniq_features
       BigDecimal.new(
-        @words.values.map(&:keys).flatten.uniq.size
+        @features.values.map(&:keys).flatten.uniq.size
       )
     end
 
--- spec/classifier/naive_bayes_spec.rb
+++ spec/classifier/naive_bayes_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe Classifier::NaiveBayes do
     let(:classifier) { Classifier::NaiveBayes.new(:sports, :not_sports) }
 
     before do
-      classifier.train(:sports, "A great game")
-      classifier.train(:not_sports, "The election was over")
-      classifier.train(:sports, "Very clean match")
-      classifier.train(:sports, "A clean but forgettable game")
-      classifier.train(:not_sports, "It was a close election")
+      classifier.train(:sports, %w{A great game})
+      classifier.train(:not_sports, %w{The election was over})
+      classifier.train(:sports, %w{Very clean match})
+      classifier.train(:sports, %w{A clean but forgettable game})
+      classifier.train(:not_sports, %w{It was a close election})
     end
 
     context "with a sport result input" do
-      let(:result) { classifier.classify("a very close game") }
+      let(:result) { classifier.classify(%w{a very close game}) }
 
       it "classifies a string as sports" do
         expect(result.category).to eq(:sports)
@@ -35,31 +35,31 @@ RSpec.describe Classifier::NaiveBayes do
 
     before do
       # https://theconversation.com/teaching-maths-what-does-the-evidence-say-actually-works-64976
-      classifier.train(:maths, "Mathematics Education Schools Teaching Math anxiety Teaching maths academic performance Better Teachers")
+      classifier.train(:maths, ["Mathematics", "Education", "Schools", "Teaching", "Math anxiety", "Teaching maths", "academic performance", "Better Teachers"])
       # https://theconversation.com/eleven-games-and-activities-for-parents-to-encourage-maths-in-early-learning-76522
-      classifier.train(:maths, "Mathematics Learning Early childhood education Parents Early childhood learning Parents role in education")
+      classifier.train(:maths, ["Mathematics", "Learnin", "Early childhood education", "Parents", "Early childhood learning", "Parents role in education"])
       # https://theconversation.com/the-goldwater-rule-prevents-psychiatrists-diagnosing-trump-from-afar-but-some-say-theres-too-much-at-stake-81674
-      classifier.train(:medical, "Psychology Psychiatry US politics Sigmund Freud Psychotherapy attention deficit hyperactivity disorder Narcissism Donald Trump Diagnosis Barry Goldwater Psychoanalysis")
+      classifier.train(:medical, ["Psychology", "Psychiatry", "US politics", "Sigmund Freud", "Psychotherapy", "attention deficit hyperactivity disorder", "Narcissism", "Donald Trump", "Diagnosis", "Barry Goldwater", "Psychoanalysis"])
       # https://theconversation.com/greg-hunts-plan-to-reduce-hospital-admissions-wont-work-if-he-cant-measure-successes-and-failures-81834
-      classifier.train(:medical, "COAG Health funding hospital funding Greg Hunt Health data health care policy")
+      classifier.train(:medical, ["COAG", "Health funding", "hospital funding", "Greg Hunt", "Health data", "health care policy"])
       # https://theconversation.com/weekly-dose-methylprednisolone-a-drug-for-treating-inflammation-but-not-rare-kidney-disease-81893
-      classifier.train(:medical, "Research Kidney disease Pharmaceuticals Arthritis Pharmacology Medication Asthma Side effects Corticosteroid Allergy Psoriasis Corticosteroids Weekly Dose")
+      classifier.train(:medical, ["Research", "Kidney disease", "Pharmaceuticals", "Arthritis", "Pharmacology", "Medication", "Asthma", "Side effects", "Corticosteroid", "Allergy", "Psoriasis", "Corticosteroids", "Weekly Dose"])
       # https://theconversation.com/to-empower-students-with-effective-writing-skills-handwriting-matters-81949
-      classifier.train(:education, "Children Teaching Primary education Secondary education NAPLAN Writing Primary school Handwriting Classrooms")
+      classifier.train(:education, ["Children", "Teaching", "Primary education", "Secondary education", "NAPLAN", "Writing", "Primary school", "Handwriting", "Classrooms"])
       # https://theconversation.com/universities-have-a-problem-with-sexual-assault-and-harassment-heres-how-to-fix-it-81096
-      classifier.train(:education, "Relationships Violence Sexual harassment Australian universities Campus sexual assault")
+      classifier.train(:education, ["Relationships", "Violence", "Sexual harassment", "Australian universities", "Campus sexual assault"])
       # https://theconversation.com/artificial-intelligence-holds-great-potential-for-both-students-and-teachers-but-only-if-used-wisely-81024
-      classifier.train(:education, "Artificial intelligence Intelligence Technology Big data data Learning analytics")
+      classifier.train(:education, ["Artificial intelligence", "Intelligence Technology", "Big data", "data", "Learning analytics"])
       # https://theconversation.com/explainer-what-is-fair-dealing-and-when-can-you-copy-without-permission-80745
-      classifier.train(:law, "Law Copyright Digital copyright Fair use Technology explainer")
+      classifier.train(:law, ["Law", "Copyright", "Digital copyright", "Fair use", "Technology explainer"])
       # https://theconversation.com/factcheck-qanda-what-are-the-facts-on-funding-for-domestic-violence-legal-services-in-australia-69214
-      classifier.train(:law, "Australian politics indigenous affairs Domestic violence Government funding Q&A factcheck Family violence Funding cuts spending cuts Intimate partner violence")
+      classifier.train(:law, ["Australian politics", "indigenous affairs", "Domestic violence", "Government funding", "Q&A", "factcheck", "Family violence", "Funding cuts", "spending cuts", "Intimate partner violence"])
       # https://theconversation.com/coupon-justice-wont-address-legal-aid-crisis-19080
-      classifier.train(:law, "Justice IPA Legal aid Legal system")
+      classifier.train(:law, ["Justice", "IPA", "Legal aid", "Legal system"])
     end
 
     context "with a sport result input" do
-      let(:result) { classifier.classify("psychoanalysis") }
+      let(:result) { classifier.classify("Pharmaceuticals") }
 
       before do
         puts result.inspect
@@ -72,10 +72,10 @@ RSpec.describe Classifier::NaiveBayes do
       it "calculates correct scores" do
         expect(result.scores).to be_a(Hash)
         expect(result.scores.size).to eq(4)
-        expect(result.scores[:maths].round(10)).to eq(BigDecimal.new("0.0014204545"))
-        expect(result.scores[:medical].round(10)).to eq(BigDecimal.new("0.0036607688"))
-        expect(result.scores[:education].round(10)).to eq(BigDecimal.new("0.002050581"))
-        expect(result.scores[:law].round(10)).to eq(BigDecimal.new("0.0020053476"))
+        expect(result.scores[:maths].round(10)).to eq(BigDecimal.new("0.001934236"))
+        expect(result.scores[:medical].round(10)).to eq(BigDecimal.new("0.0049586777"))
+        expect(result.scores[:education].round(10)).to eq(BigDecimal.new("0.0027548209"))
+        expect(result.scores[:law].round(10)).to eq(BigDecimal.new("0.0027548209"))
       end
     end
   end
